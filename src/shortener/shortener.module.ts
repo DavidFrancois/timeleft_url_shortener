740c72dd7a88dff4589a807450646c52cdcfee1c
Francis <francois.david107@gmail.com>
2025-12-18 15:47:39 +0100

    feat(api): added a throttler to the shortener module

--- src/shortener/shortener.module.ts
+++ src/shortener/shortener.module.ts
@@ -2,8 +2,19 @@ import { Module } from '@nestjs/common';
 import { ShortenerService } from './shortener.service';
 import { ShortenerController } from './shortener.controller';
 import { PrismaService } from 'src/standalones/prisma.service';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
+  imports: [
+    ThrottlerModule.forRoot({
+      throttlers: [
+        {
+          ttl: 60000,
+          limit: 30,
+        },
+      ],
+    }),
+  ],
   providers: [ShortenerService, PrismaService],
   controllers: [ShortenerController],
 })
