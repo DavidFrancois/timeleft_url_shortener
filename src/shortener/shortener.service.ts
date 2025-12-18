c14c375cc0f372f93786db26b22dcb8dc6e03448
Francis <francois.david107@gmail.com>
2025-12-18 15:41:43 +0100

    fix(api): now relying on prisma rather than manual checking for code availability

--- src/shortener/shortener.service.ts
+++ src/shortener/shortener.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import { Prisma } from 'src/generated/prisma/client';
 import { PrismaService } from 'src/standalones/prisma.service';
 import { randomShortCode } from 'src/utils/shortCode.utils';
 
@@ -6,52 +7,43 @@ import { randomShortCode } from 'src/utils/shortCode.utils';
 export class ShortenerService {
   constructor(private readonly prisma: PrismaService) {}
 
-  private async findUniqueShortCode(): Promise<string> {
-    const maxAttempts = process.env.MAX_SHORTCODE_GENERATION_ATTEMPTS;
-    let attempts = 0;
-    let exists = false;
-    let shortCode: string;
-
-    do {
-      shortCode = randomShortCode(6);
-      const res = await this.prisma.url.findUnique({
-        where: { short: shortCode },
-      });
-
-      if (res) {
-        exists = true;
-      } else {
-        exists = false;
-      }
-
-      ++attempts;
-    } while (exists && attempts < (maxAttempts ? Number(maxAttempts) : 5));
-
-    return shortCode;
-  }
-
   async shortenUrl(request: Request, shortenUrlDto: any): Promise<string> {
     const { originalUrl, ttl } = shortenUrlDto;
 
     const apiUrl = process.env.API_URL || 'http://localhost:8080/';
 
     // Generate a short url that don't collide with existing ones
-    const shortCode = await this.findUniqueShortCode();
+    let newEntry;
+    const maxAttempts = parseInt(
+      process.env.MAX_SHORTCODE_GENERATION_ATTEMPTS || '5',
+      10,
+    );
 
-    if (!shortCode) {
-      throw new Error('Could not generate a unique short code');
+    for (let attempt = 0; attempt < maxAttempts; attempt++) {
+      const shortCode = randomShortCode();
+
+      try {
+        newEntry = await this.prisma.url.create({
+          data: {
+            original: originalUrl,
+            short: shortCode,
+            ttl: ttl
+              ? BigInt(ttl)
+              : BigInt(process.env.DEFAULT_TTL_MINUTES || '60'),
+          },
+        });
+      } catch (err) {
+        if (
+          err instanceof Prisma.PrismaClientKnownRequestError &&
+          err.code === 'P2002'
+        ) {
+          continue;
+        }
+
+        throw err;
+      }
     }
 
-    const written = await this.prisma.url.create({
-      data: {
-        original: originalUrl,
-        short: shortCode,
-        ttl: ttl
-          ? BigInt(ttl)
-          : BigInt(process.env.DEFAULT_TTL_MINUTES || '60'),
-      },
-    });
-
-    return `${apiUrl}${written.short}`;
+    return `${apiUrl}${newEntry.short}`;
   }
 }
